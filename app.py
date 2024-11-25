fbaa6513b3c0dc489eb66d10d5a6de7614c829c1
Siriyakorn S. <gssuepian@gmail.com>
2024-11-25 23:38:19 +0100

    Update app.py

--- app.py
+++ app.py
@@ -42,10 +42,10 @@ text = "#110301"
 # Text: #110301
 
 # # Use local CSS
-# def local_css(file_name):
-#     with open(file_name) as f:
-#         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
-# local_css("style/style.css")
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+local_css("style/style.css")
 
 # Formatting buttons with CSS
 st.markdown("""
